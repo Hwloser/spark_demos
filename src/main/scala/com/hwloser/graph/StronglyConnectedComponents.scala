033f09e4adf2547b901a687cd1e3d0849be71cf2
hwloser <hwloser@163.com>
2021-07-01 19:16:44 +0800

    [UPDATE] update StronglyConnectedComponents test demo

--- src/main/scala/com/hwloser/graph/StronglyConnectedComponents.scala
+++ src/main/scala/com/hwloser/graph/StronglyConnectedComponents.scala
@@ -1,8 +1,18 @@
 package com.hwloser.graph
 
+import org.apache.spark.sql.SparkSession
+
 class StronglyConnectedComponents {
 
   def main(args: Array[String]): Unit = {
+    val ss = SparkSession
+      .builder()
+      .appName(s"${getClass.getSimpleName}")
+      .master("local[*]")
+      .getOrCreate()
+
+    val sc = ss.sparkContext
+
 
   }
 }
