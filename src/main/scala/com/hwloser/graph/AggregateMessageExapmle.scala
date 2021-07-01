fcf5fb3fc19dd0bf6be31ae09930bd2d8ad637b8
hwloser <hwloser@163.com>
2021-07-01 18:36:09 +0800

    [NEW] add aggregate message demo

--- src/main/scala/com/hwloser/graph/AggregateMessageExapmle.scala
+++ src/main/scala/com/hwloser/graph/AggregateMessageExapmle.scala
@@ -1,8 +1,8 @@
 package com.hwloser.graph
 
+import org.apache.spark.graphx.VertexRDD
 import org.apache.spark.graphx.util.GraphGenerators
 import org.apache.spark.sql.SparkSession
-
 
 
 object AggregateMessageExapmle {
@@ -23,10 +23,48 @@ object AggregateMessageExapmle {
     graph
       .cache()
       .triplets
-      .collect()
+      .take(10)
       .foreach(println)
 
+    val olderFollowers: VertexRDD[(Int, Double)] = graph
+      .aggregateMessages[(Int, Double)](
+        triplet => {
+          // Map Function
+          if (triplet.srcAttr > triplet.dstAttr) {
+            // send message to destination vertex containing counter and age
+            triplet.sendToDst((1, triplet.srcAttr))
+          }
+        },
+        // add counter and age
+        (a, b) => {
+          // reduce function
+          (a._1 + b._1, a._2 + b._2)
+        }
+      )
 
+    println("olderFollowers -- ")
+    olderFollowers
+      .cache()
+      .takeOrdered(10)
+      .foreach(println)
+
+    // Divide total age by number of older followers
+    // to get average of older followers
+    val avgAgeOfOlderFollowers = olderFollowers
+      .mapValues { (vertextId, vertexData) =>
+        vertexData match {
+          case (count, totalAge) => {
+            totalAge / count
+          }
+        }
+      }
+
+    println("avgAgeOfOlderFollowers -- ")
+    avgAgeOfOlderFollowers
+      .take(10)
+      .foreach(println)
+
+    spark.stop()
 
   }
 }
